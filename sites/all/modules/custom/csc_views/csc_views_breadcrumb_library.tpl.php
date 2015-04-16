6761b380b7d8af7e5f51ea93d1a5f21a8c9331e5
John DeMott <jd7wd@drupal-dev-s.shanti.virginia.edu>
2015-04-16 19:15:45 -0400

    Removing corrupted repo

--- sites/all/modules/custom/csc_views/csc_views_breadcrumb_library.tpl.php
+++
@@ -1,8 +0,0 @@
-<div class="library-dropdown" style="display: none;">
-  <div class="inner-library">
-    <div class="view-all-library-container">
-      <?php print $value['all_source_link']; ?>
-    </div>
-    <?php print $value['library_list']; ?>
-  </div>
-</div>
