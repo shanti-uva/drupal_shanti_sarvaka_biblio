fb10ba30718e530ed28978160324277b84b90df1
John De Mott <heydemo13@gmail.com>
2015-05-29 19:49:54 -0500

    Making sure clicking a tag from individual page goes to collection view mode, fixing search fly-out default status bug

--- js/shanti-search-csc-theme.js
+++ js/shanti-search-csc-theme.js
@@ -1,16 +1,18 @@
 (function ($) {
   Drupal.behaviors.cscTheme = {
     attach: function (context, settings) {
+      var flyout_status = $.cookie('flyout_status');
       // Keep search flyout open
       if ($("#search-flyout").length) {
-        var status = $.cookie('flyout-status');
-        if ($.cookie('flyout-status') && $.cookie('flyout-status') == 'open') {
+        var status = $.cookie('flyout_status');
+        if ($.cookie('flyout_status') && $.cookie('flyout_status') == 'open') {
           openSearchFlyout();
         }
         else {
           closeSearchFlyout();
         }
       }
+
       $('#search-flyout div.flap').click(function(e) {
         if ($('#search-flyout').attr('isopened')) {
           var flyout_status = 'open';
@@ -20,8 +22,7 @@
           var flyout_status = 'close';
           $('.page-csc-search .main-content .content-section .view-content, #block-csc-views-custom-sort-filter,.view-biblio-search-api .attachment').animate({width: '100%'});
         }
-        $.removeCookie('flyout-status');
-        $.cookie('flyout-status', flyout_status);
+        $.cookie('flyout_status', flyout_status);
         e.preventDefault();
       });
     }
