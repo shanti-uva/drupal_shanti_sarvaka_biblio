15deb50fd6e2ad47decab4aef6696265f3fa3bc2
John De Mott <heydemo13@gmail.com>
2015-09-15 17:16:59 +0700

    Fixing theme to support new fancytree widgets

--- template.php
+++ template.php
@@ -43,45 +43,40 @@ function sources_theme_remove_front_page_class(&$variables) {
  */
 function sources_theme_preprocess_views_view_fields(&$vars) {
   if ($vars['view']->name == 'biblio_search_api') {
-    $year = (!empty($vars['row']->_entity_properties['biblio_year'])) ? $vars['row']->_entity_properties['biblio_year'] : '';
-    $parameters = sources_theme_url_parameters($vars);
-    $publication_type = sources_theme_publication_type($vars);
-    $title_info = sources_theme_content_title_info($year, $publication_type);
-    $custom_link_wrapper_prefix = '<div class="source-icon-' . $publication_type . ' title-link-container"><span class="glyphicon shanticon-essays"></span>';
-    $custom_link_wrapper_suffix = '</div>';
-
+    $publication_year = (!empty($vars['row']->_entity_properties['biblio_year'])) ? $vars['row']->_entity_properties['biblio_year'] : '';
+    $updated_query_string_parameters = sources_theme_get_updated_query_string_parameters($vars);
+    $publication_format = sources_theme_get_publication_type($vars);
+    $source_title_info = sources_theme_get_source_title_info($publication_year, $publication_format);
+    $custom_title_link_wrapper_prefix = '<div class="source-icon-' . $publication_format . ' title-link-container"><span class="glyphicon shanticon-essays"></span>';
+    $custom_title_link_wrapper_suffix = '</div>';
+    //Display custom views field output based on current views display.
     switch ($vars['view']->current_display) {
       case 'page':
-        $link = l($vars['row']->_entity_properties['title'] . $title_info, 'sources-search/biblio', array('query' => $parameters, 'html' => TRUE));
-        $vars['fields']['title']->content = $custom_link_wrapper_prefix . $link . $custom_link_wrapper_suffix;
+        $link = l($vars['row']->_entity_properties['title'] . $source_title_info, 'sources-search/biblio', array('query' => $updated_query_string_parameters, 'html' => TRUE));
+        $vars['fields']['title']->content = $custom_title_link_wrapper_prefix . $link . $custom_title_link_wrapper_suffix;
         break;
       case 'biblio_full':
-        $title = $vars['row']->_entity_properties['title'] . $title_info;
-        $vars['fields']['title']->content = $custom_link_wrapper_prefix . $title . $custom_link_wrapper_suffix;;
+        $vars['fields']['biblio_publication_type_1']->content = '<span>' . $publication_format . '</span>';
         break;
     }
   }
 }
 
 /**
- * Returns query parameters for source content link.
+ * Returns query string parameters with updated node id and page number.
  *
  */
-function sources_theme_url_parameters($vars) {
+function sources_theme_get_updated_query_string_parameters($vars) {
   $current_page = (!empty($_GET['page'])) ? intval($_GET['page']) : 0;
-  $page = $vars['view']->row_index + (25 * $current_page);
-  $query_strings = $_SERVER['QUERY_STRING'];
-  parse_str($query_strings, $query_string_values);
-  $parameters = array_replace($query_string_values, array('page' => $page));
+  $new_page = $vars['view']->row_index + (25 * $current_page);
+  $current_query_strings = $_SERVER['QUERY_STRING'];
+  parse_str($current_query_strings, $parsed_current_query_strings);
+  $parameters = array_replace($parsed_current_query_strings, array('page' => $new_page));
   $parameters['current_nid'] = $vars['row']->entity;
   return $parameters;
 }
 
-/**
- * Returns publication type.
- *
- */
-function sources_theme_publication_type($vars) {
+function sources_theme_get_publication_type($vars) {
   $publication_type_name = '';
   if (!empty($vars['row']->_entity_properties['biblio_publication_type'])) {
     $publication_type_id = $vars['row']->_entity_properties['biblio_publication_type'];
@@ -91,27 +86,27 @@ function sources_theme_publication_type($vars) {
 }
 
 /**
- * Returns year and publication type information.
+ * Returns source title with publication year and format.
  *
  */
-function sources_theme_content_title_info($year, $publication_type) {
-  if (!empty($publication_type)) {
-    $publication_type = '<span class="publication-type">' . $publication_type . '</span>';
+function sources_theme_get_source_title_info($year, $publication_format) {
+  if (!empty($publication_format)) {
+    $publication_format = '<span class="publication-type">' . $publication_format . '</span>';
   }
-  $publication_info = array($year, $publication_type);
-  $information_output = '';
+  $publication_info = array($year, $publication_format);
+  $source_title_info = '';
   $info_index = 1;
   foreach ($publication_info as $info) {
     if (!empty($info)) {
       if ($info_index > 1) {
-        $information_output .= ', ' . $info;
+        $source_title_info .= ', ' . $info;
       }
       else {
-        $information_output .= $info;
+        $source_title_info .= $info;
       }
       $info_index++;
     }
   }
-  $information_output = ' (' . $information_output . ')';
-  return $information_output;
+  $source_title_info = ' (' . $source_title_info . ')';
+  return $source_title_info;
 }
